3478793fcf7585fa2f56df68ecbd438ae9e3e690
KillianDudduss <Killian.dusautiez@laposte.net>
2018-01-30 01:43:06 +0100

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+	bot.user.setActivity("TutoBot, !help");
+	console.log("Le bot a bien été connecté");
+})
+
+bot.login("NDA3Njg5ODQ1MTE3NTUwNTky.DVFNsw.v1a4ej6CrRvyQeAx7WXYlPwbKcw");

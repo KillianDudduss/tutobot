64273d4feb0387b0f8f74968be5947ab4b9fccf0
KillianDudduss <Killian.dusautiez@laposte.net>
2018-01-30 02:14:20 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
+const config = require("./config.json");
+
 bot.on("ready", function() {
 	bot.user.setActivity("TutoBot, !help");
 	console.log("Le bot a bien été connecté");
